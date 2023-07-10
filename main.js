0cd9f501587246e610fb28a95a09d87bb34ae9d7
Luis Prado <kaeltas898@gmail.com>
2023-07-09 21:25:30 -0500

    Add. Carrito de compras e interaccion sobre ventanas

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
+//* variables para la vista desktop
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+//* variables para la vista mobile
 const iconMenuMobile = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+//* variables para la option del carrito de compras
+const iconCarrito = document.querySelector('.navbar-shopping-cart');
+const menuCarritoIcon = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 iconMenuMobile.addEventListener('click', toggleMobileMenu);
+iconCarrito.addEventListener('click', toggleIconCarrito);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    menuCarritoIcon.classList.add("inactive");
 }
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    menuCarritoIcon.classList.add("inactive");
+}
+function toggleIconCarrito() {
+    //? si mobileMenu esta cerrado
+    // const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    // if (!isMobileMenuClosed) {
+    //     mobileMenu.classList.add("inactive");
+    // }
+    menuCarritoIcon.classList.toggle('inactive');
+    desktopMenu.classList.add("inactive");
 }
